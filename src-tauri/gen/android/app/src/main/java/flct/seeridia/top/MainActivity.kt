44fa7071c1b9e95d409969d327f7c7e8587fad71
Seeridia <Seeridia@gmail.com>
2026-01-26 19:12:30 +0800

    feat: 增加 android 支持

---
+++ src-tauri/gen/android/app/src/main/java/flct/seeridia/top/MainActivity.kt
@@ -0,0 +1,11 @@
+package flct.seeridia.top
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
